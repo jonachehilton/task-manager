fb06706b4cbec00037e3a7c28c8262050e2e1b06
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2021-12-31 16:37:48 +1000

    Add project resolver

--- src/modules/project.js
+++ src/modules/project.js
@@ -22,3 +22,19 @@ export const typeDef = gql`
     addUserToProject(projectId: ID!, userId: ID!): Project!
   }
 `;
+
+export const resolvers = {
+  Project: {
+    id: ({ _id, id }) => _id || id,
+    progress: async ({ _id }, _, { db }) => {
+      const tasks = await db.collection('Tasks').find({ projectId: _id }).toArray();
+      const completedTasks = tasks.filter(Task => Task.completed);
+
+      if (tasks.length === 0) return 0;
+
+      return (completedTasks.length / tasks.length) * 100;
+    },
+    users: async ({ userIds }, _, { db }) => await Promise.all(userIds.map(userId => db.collection('Users').findOne({ _id: ObjectId(userId) }))) ,
+    tasks: async ({ _id }, _, { db }) => await db.collection('Tasks').find({ projectId: ObjectId(_id) }).toArray(),
+  },
+}
