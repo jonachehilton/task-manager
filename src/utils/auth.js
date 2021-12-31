792ea0a9c5bd466c43e8dca28bb95e546b74d506
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2021-12-31 17:45:35 +1000

    Add getUserFromToken helper

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,3 +1,13 @@
 const jwt = require('jsonwebtoken');
+const { ObjectId } = require('mongodb');
 
 export const getToken = (user) => jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7 days' });
+
+export const getUserFromToken = async (token, db) => {
+  if (!token) return null;
+
+  const tokenData = jwt.verify(token, JWT_SECRET);
+  if (!tokenData?.id) return null;
+
+  return await db.collection('Users').findOne({ _id: ObjectId(tokenData.id) });
+}
